6676fd105a99ed3c992df0a59f4c9b01c4301d4d
emdupre <emd222@cornell.edu>
2017-05-27 17:30:17 -0400

    Update __init__.py files

--- meica/tests/test_meica.py
+++ meica/tests/test_meica.py
@@ -1,11 +1,11 @@
-from meica import format_inset, fparse
+from meica.meica import format_inset, fparse
 
 def test_fparse():
     ('sub.001', '.nii.gz') == fparse('sub.001.nii.gz')
     ('sub.001', '+tlrc')   == fparse('sub.001+tlrc.BRIK.gz')
 
 
-def test_format_inputs():
+def test_format_inset():
     ('sub_001.e02.nii.gz',
      'sub_001.e0123') == format_inset('sub_001.e0[1,2,3].nii.gz')
 
